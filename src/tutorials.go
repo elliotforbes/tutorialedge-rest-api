42b6d995c724efe1d27cc27fa5024faeca94d938
= <emforce@live.com>
2017-01-11 22:38:17 +0000

    made start on tutorials apis

--- src/tutorials.go
+++ src/tutorials.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -15,41 +14,55 @@ import (
  * Tutorial... - standard tag used in frontend
  */
 type Tutorial struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Desc      string `json:"description"`
+	Body      string `json:"body"`
+	IsLive    int    `json:"isLive"`
+	Author    string `json:"author"`
+	Slug      string `json:"slug"`
+	ImagePath string `json:"image_path"`
 }
 
 func AllTutorials(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "All Tags!")
+	db := connect()
+	defer db.Close()
+
+	var tutorials []Tutorial
+	results, err := db.Query("SELECT id, title, description, body, slug, isLive, author, image_path FROM lessons")
+
+	for results.Next() {
+		var tutorial Tutorial
+		err = results.Scan(&tutorial.ID, &tutorial.Title, &tutorial.Desc, &tutorial.Body, &tutorial.Slug, &tutorial.IsLive, &tutorial.Author, &tutorial.ImagePath)
+		if err != nil {
+			json.NewEncoder(w).Encode(HttpResp{Status: 200, Description: "Failed to select all from tutorials"})
+		}
+		tutorials = append(tutorials, tutorial)
+	}
+
+	json.NewEncoder(w).Encode(tutorials)
 }
 
 func GetTutorial(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	tagID, err := strconv.Atoi(vars["id"])
-
+	tutorialID, err := strconv.Atoi(vars["id"])
 	if err != nil {
 		fmt.Fprintln(w, "Not a Valid id")
 	}
-
 	// Open up our database connection.
-	db, err := sql.Open("mysql", "root:charlie1@tcp(127.0.0.1:3306)/tuts")
-
-	// if there is an error opening the connection, handle it
-	if err != nil {
-		log.Print(err.Error())
-	}
+	db := connect()
 	defer db.Close()
 
-	var tag Tag
+	var tutorial Tutorial
 	// Execute the query
-	err = db.QueryRow("SELECT id, name FROM tags where id = ?", tagID).Scan(&tag.ID, &tag.Name)
+	err = db.QueryRow("SELECT id, title, description, body, slug, isLive, author, image_path FROM lessons where id = ?", tutorialID).Scan(&tutorial.ID, &tutorial.Title, &tutorial.Desc, &tutorial.Body, &tutorial.Slug, &tutorial.IsLive, &tutorial.Author, &tutorial.ImagePath)
 	if err != nil {
 		log.Print(err.Error()) // proper error handling instead of panic in your app
 		fmt.Fprintln(w, err.Error())
 		return
 	}
 
-	json.NewEncoder(w).Encode(tag)
+	json.NewEncoder(w).Encode(tutorial)
 }
 
 func InsertTutorial(w http.ResponseWriter, r *http.Request) {
